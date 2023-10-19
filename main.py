4ee872e2e76855e4e867823a3628d408421b3778
alrab223 <allbear826@gmail.com>
2023-10-19 14:42:32 +0900

    コグの読み込み処理を変更

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import glob
 import os
 import traceback
 
@@ -6,10 +5,10 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-cog_path = glob.glob("cog/*.py")
-cog_path = [x.replace("/", ".") for x in cog_path]
-cog_path = [x.replace(".py", "") for x in cog_path]
-INITIAL_EXTENSIONS = cog_path
+# 環境変数を使うための設定
+base_path = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(base_path, '.env')
+load_dotenv(dotenv_path)
 
 
 class MyBot(commands.Bot):
@@ -17,18 +16,15 @@ class MyBot(commands.Bot):
    def __init__(self, command_prefix, intents):
 
       super().__init__(command_prefix, intents=intents)
-
-      for cog in INITIAL_EXTENSIONS:
-         try:
-            self.load_extension(cog)
-         except Exception:
-            traceback.print_exc()
+      for filename in os.listdir('cog'):
+         if filename.endswith('.py'):
+            try:
+               self.load_extension(f'cog.{filename[:-3]}')
+            except Exception:
+               traceback.print_exc()
 
 
 if __name__ == '__main__':
    intents = discord.Intents.all()
    bot = MyBot(command_prefix='$', intents=intents)
-   base_path = os.path.dirname(os.path.abspath(__file__))
-   dotenv_path = os.path.join(base_path, '.env')
-   load_dotenv(dotenv_path)
    bot.run(os.getenv("BOT"))
